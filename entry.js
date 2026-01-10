30cf100f0d8eab1e0d49b523e47ce0df0014cb14
Danny <danny.belmonte4@gmail.com>
2026-01-10 17:42:49 -0600

    Co-authored-by: Abel071895 <Abel071895@users.noreply.github.com>

---
+++ entry.js
@@ -0,0 +1,28 @@
+import { Server } from "socket.io";
+
+const myServer = new Server(3001, {
+    cors: {
+        origin: "*",
+    }
+});
+
+console.log("Tic Tac Toe server is running on port 3001");
+
+const playersConnected = [];
+
+
+
+myServer.on("connection", (socket) => {
+    console.log("New client connected:", socket.id);
+    socket.on('test-message', (data) => console.log(data))
+
+    socket.on('registerPlayer',(playerName) => {
+        console.log(`Player registered: ${playerName}`);
+        playersConnected.push(playerName);
+        socket.emit('playersConnected', playersConnected);
+    })
+})
+
+
+
+
